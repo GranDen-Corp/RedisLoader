79b8a01ec876ed5d5367f9f6c07ad542fab84d44
Chen Yu Pao <windperson@gmail.com>
2019-05-22 17:08:29 +0800

    update demo code

--- test_sample/VerifyRedisLoaderWebApp/Controllers/TestOptionController.cs
+++ test_sample/VerifyRedisLoaderWebApp/Controllers/TestOptionController.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using VerifyRedisLoaderWebApp.TypedOptions;
+using webapi.Models.PocketHiGameDb.YamlDb;
 
 namespace VerifyRedisLoaderWebApp.Controllers
 {
@@ -18,7 +19,7 @@ namespace VerifyRedisLoaderWebApp.Controllers
     public class TestOptionController : ControllerBase
     {
         private ServiceProvider _container;
-
+        private IConfigurationSection _stickerRewardSection;
         private readonly MissionRarityOption _missionRarity;
         private readonly MissionTimeOption _missionTimeOption;
 
@@ -32,12 +33,17 @@ namespace VerifyRedisLoaderWebApp.Controllers
 
             _missionRarity = missionRarityOptionAccessor.Value;
             _missionTimeOption = missionTimeOptionAccessor.Value;
+            
         }
 
         private static IConfiguration CreateRedisConfiguration()
         {
             var configBuilder = new ConfigurationBuilder();
             configBuilder.AddRedis("localhost:6379", "yaml_config", true, "aligala_curDbNum");
+            
+            //configBuilder.AddRedis("aligala-gametable-stage.redis.cache.windows.net:6380,password=4cRFQqlw38TZ0oz7ABDBk9lfmkgWieZKWeu5jchSzkY=,ssl=True,abortConnect=False",
+                //"aligala_game_table", true, "aligala_curDbNum");
+
             return configBuilder.Build();
         }
 
@@ -45,20 +51,34 @@ namespace VerifyRedisLoaderWebApp.Controllers
         {
             var serviceCollection = new ServiceCollection();
             serviceCollection.Configure<MissionMainDataOption>(configuration.GetSection("Mission_MainData"));
+            //serviceCollection.Configure<List<StickerRewardDataYamlEntity>>(configuration.GetSection("Sticker_Reward_Data"));
+
+            _stickerRewardSection = configuration.GetSection("Sticker_Reward_Data");
+
             return serviceCollection.BuildServiceProvider();
         }
 
         [Route("tableData")]
         public IActionResult GetTableData()
         {
-            var option = _container.GetService<IOptionsMonitor<MissionMainDataOption>>();
-            var mData = option.CurrentValue;
+            //var missionDataOption = _container.GetService<IOptionsMonitor<MissionMainDataOption>>();
+            //var mData = missionDataOption.CurrentValue;
+
+            //var stickerRewardDataOption = _container.GetService<IOptionsMonitor<List<StickerRewardDataYamlEntity>>>();
+            //var stickerData = stickerRewardDataOption.CurrentValue;
+
+            var reward004Section = _stickerRewardSection.GetSection("Sticker_Page_Reward_004");
+
+            var stickerData = new StickerRewardDataYamlEntity();
+
+            reward004Section.Bind(stickerData);
 
             return Ok(new
             {
-                missionMainData = mData,
-                missionRarity = _missionRarity,
-                missionTimeOption = _missionTimeOption
+                //missionMainData = mData,
+                //missionRarity = _missionRarity,
+                //missionTimeOption = _missionTimeOption,
+                stickerRewardData = stickerData
             });
         }
 
---
+++ test_sample/VerifyRedisLoaderWebApp/TypedOptions/StickerRewardDataYamlEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace webapi.Models.PocketHiGameDb.YamlDb
+{
+    public class StickerRewardDataYamlEntity
+    {
+        public string ID { get; set; }
+        public string Reward_1_Type { get; set; }
+        public string Reward_1_ID { get; set; }
+        public int Reward_1_Amount { get; set; }
+        public string Reward_2_Type { get; set; }
+        public string Reward_2_ID { get; set; }
+        public int? Reward_2_Amount { get; set; }
+        public string Reward_3_Type { get; set; }
+        public string Reward_3_ID { get; set; }
+        public int? Reward_3_Amount { get; set; }
+    }
+}
